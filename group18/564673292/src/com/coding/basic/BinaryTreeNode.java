1984f8c3c4404e1076f95757b01efb38ff26908d
lqt0223 <lqt19920223@gmail.com>
2017-03-15 16:34:44 +0800

    第三周作业

--- group18/564673292/src/com/coding/basic/BinaryTreeNode.java
+++ group18/564673292/src/com/coding/basic/BinaryTreeNode.java
@@ -1,7 +1,3 @@
-// This is a node in a customized binaryTree. The tree have 2 extra features comparing to general binary trees.
-// 1. The data of each node are in number class.
-// 2. The left child node has a smaller number data than root node, and the right child node has a larger number data that root node.
-
 package com.coding.basic;
 
 public class BinaryTreeNode<E> {
@@ -44,7 +40,6 @@ public class BinaryTreeNode<E> {
     public boolean setRight(BinaryTreeNode<E> right) {
         if(this.compareWithRoot(right.data) <= 0 || this.right != null) {
             System.err.println("The right node data should be larger than root node.");
-
             return false;
         }else{
             this.right = right;

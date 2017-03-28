f06842f0496e618f0a71f3b6b45429953fc8ced6
sshfuture <hushuai1984>
2017-03-29 03:31:54 +0800

    LRU算法数据结构 作业提交

--- group18/744888802/src/main/java/com/coding/basic/linklist/LRUPageFrame.java
+++ group18/744888802/src/main/java/com/coding/basic/linklist/LRUPageFrame.java
@@ -6,6 +6,7 @@ package com.coding.basic.linklist;
  *
  */
 public class LRUPageFrame {
+
 	
 	private static class Node {
 		
@@ -15,6 +16,19 @@ public class LRUPageFrame {
 
 		Node() {
 		}
+		@Override
+		public boolean equals(Object object) {
+			Node node = (Node)object;
+			return this.pageNum==node.pageNum;
+		}
+	}
+
+	public static void main(String[] args) {
+		Node node1 = new Node();
+		Node node2 = new Node();
+		node1.pageNum = 1;
+		node2.pageNum = 1;
+		System.out.println(node1.equals(node2));
 	}
 
 	private int capacity;
@@ -22,6 +36,8 @@ public class LRUPageFrame {
 	
 	private Node first;// 链表头
 	private Node last;// 链表尾
+
+	private int size ;
 
 	
 	public LRUPageFrame(int capacity) {
@@ -33,12 +49,77 @@ public class LRUPageFrame {
 	/**
 	 * 获取缓存中对象
 	 * 
-	 * @param key
+	 * @param
 	 * @return
 	 */
 	public void access(int pageNum) {
-		
-	
+
+		Node node = new Node();
+		node.pageNum = pageNum;
+		if(first == null){
+			first = node;
+			last = node;
+			this.size++;
+			return;
+		}
+
+		boolean hasValue = moveUp(node);
+		if(hasValue){
+			return;
+		}
+
+		if(this.size == this.capacity){
+			removeLast();
+		}else{
+			this.size++;
+		}
+		first.prev = node;
+		node.next = first;
+		first = node;
+	}
+
+	private void removeLast(){
+		last = last.prev;
+		last.next.prev = null;
+		last.next = null;
+	}
+
+
+
+	private boolean moveUp(Node node){
+		Node thisNode = first;
+		for(int i=0;i<size;i++){
+			if(node.equals(thisNode)){
+				if(thisNode.equals(first)){
+					return true;
+				}
+				if(thisNode.equals(last)){
+					thisNode.next = first;
+					first.prev = thisNode;
+
+					first = thisNode;
+
+					thisNode.prev.next = null;
+					last = thisNode.prev;
+					thisNode.prev = null;
+
+					return true;
+				}
+				thisNode.prev.next = thisNode.next;
+				thisNode.next.prev = thisNode.prev;
+
+				first.prev = thisNode;
+				thisNode.next = first;
+				thisNode.prev = null;
+				first = thisNode;
+				return true;
+			}
+			thisNode = thisNode.next;
+		}
+
+		return false;
+
+
 	}
 	
 	
